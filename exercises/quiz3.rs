99002aedf91c9bbcd4c3abde7e7fd040b36ae1e9
Caleb L'Italien <caleblitalien@proton.me>
2023-10-23 09:27:12 -0400

    finished quiz 3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,8 +16,6 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub struct ReportCard {
     pub grade: f32,
     pub student_name: String,
@@ -25,9 +23,33 @@ pub struct ReportCard {
 }
 
 impl ReportCard {
-    pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+    pub fn print(&self, grade_type: &str) -> String {
+        let grade = &self.grade;
+        if grade_type == "letter"{
+            format!("{} ({}) - achieved a grade of {}",
+                &self.student_name, &self.student_age, Self::convert_to_letter_grade(self.grade))
+        }
+        else {
+            format!("{} ({}) - achieved a grade of {}",
+                &self.student_name, &self.student_age, &self.grade)
+        }
+    }
+    fn convert_to_letter_grade(grade: f32) -> &'static str {
+        if grade < 0.0 {
+            return "Invalid Grade";
+        }
+        match grade {
+            _ if grade < 5.5 => "F-",
+            _ if grade >= 5.5 && grade < 6.0 => "F+",
+            _ if grade >= 6.0 && grade < 6.5 => "D-",
+            _ if grade >= 6.5 && grade < 7.0 => "D+",
+            _ if grade >= 7.0 && grade < 7.5 => "C-",
+            _ if grade >= 7.5 && grade < 8.0 => "C+",
+            _ if grade >= 8.0 && grade < 8.5 => "B-",
+            _ if grade >= 8.5 && grade < 9.0 => "B+",
+            _ if grade >= 9.0 && grade < 9.5 => "A-",
+            _ => "A+",
+        }
     }
 }
 
@@ -43,21 +65,20 @@ mod tests {
             student_age: 12,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print("numeric"),
             "Tom Wriggle (12) - achieved a grade of 2.1"
         );
     }
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: 10.0,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
         assert_eq!(
-            report_card.print(),
+            report_card.print("letter"),
             "Gary Plotter (11) - achieved a grade of A+"
         );
     }
